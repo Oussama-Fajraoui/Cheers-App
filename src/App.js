2a73139bf66bd39ddc6b36e047580eca1ecc9632
Oussama Fajraoui <102801929+Oussama-Fajraoui@users.noreply.github.com>
2023-01-30 10:36:51 +0100

    adding bootstrap setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AppRouter from "./router/AppRouter";
+import ContextProvider from './context/ContextProvider'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextProvider>
+      <AppRouter>
+        <div className="App">
+          <h1>Hello</h1>
+        </div>
+      </AppRouter>
+    </ContextProvider>
   );
 }
 
